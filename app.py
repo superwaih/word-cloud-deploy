0224ae9e2e39ecb97fcb671a5b9588c1d5cdd6f7
Adewale shittu <sheriffhardewale@gmail.com>
2023-11-15 10:02:29 +0100

    feat: restored back to old function

--- app.py
+++ app.py
@@ -1,46 +1,57 @@
 from flask import Flask, render_template, request, send_file
 import numpy as np
 from PIL import Image
-from wordcloud import WordCloud, STOPWORDS
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+import matplotlib.pyplot as plt
 from io import BytesIO
-from flask_caching import Cache
 
 app = Flask(__name__)
-cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 
 stopwords = set(STOPWORDS)
 stopwords.update(["drink", "now", "wine", "flavor", "flavors"])
-
-@cache.memoize(timeout=3600)  # Cache timeout in seconds (1 hour in this example)
-def generate_wordcloud(wine_mask_np, text_input):
-    # Your existing word cloud generation code here
-    # ...
-
-    wc.generate(text_input.upper())
-
-    img_buffer = BytesIO()
-    wc.to_image().save(img_buffer, format='PNG')
-    img_buffer.seek(0)
-
-    return img_buffer
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/generate_wordcloud', methods=['POST'])
-def generate_wordcloud_route():
+def generate_wordcloud():
     if 'wine_mask' not in request.files:
         return "No wine mask provided."
 
     wine_mask = request.files['wine_mask']
     wine_mask_np = np.array(Image.open(wine_mask))
 
-    text_input = request.form.get('text_input', '')
+    def transform_format(val):
+        return 255 if val != 0 else val
 
-    img_buffer = generate_wordcloud(wine_mask_np, text_input)
+    vectorized_transform_format = np.vectorize(transform_format)
+    transformed_wine_mask = vectorized_transform_format(wine_mask_np)
+
+    def one_color_func(word=None, font_size=None, position=None, orientation=None, font_path=None, random_state=None):
+        h, s, l = 0, 0, 0
+        return "hsl({}, {}%, {}%)".format(h, s, l)
+
+    text_input = request.form.get('text_input', '')
+    
+    wc = WordCloud(
+        background_color="white",
+        mask=transformed_wine_mask,
+        stopwords=stopwords,
+        max_words=200,
+        repeat=True,
+        color_func=one_color_func
+    )
+
+    wc.generate(text_input.upper())
+
+    # Save the word cloud image to a BytesIO object
+    img_buffer = BytesIO()
+    wc.to_image().save(img_buffer, format='PNG')
+    img_buffer.seek(0)
 
     return send_file(img_buffer, mimetype='image/png', as_attachment=True, download_name='wordcloud.png')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
