f78af7a1bfe0ce90535db07d7ec53d52416adfe8
Adewale shittu <sheriffhardewale@gmail.com>
2024-02-06 04:32:56 +0100

    added changes

--- app.py
+++ app.py
@@ -1,26 +1,37 @@
-from flask import Flask, render_template, request, send_file
+from flask import Flask, request, send_file
 import numpy as np
 from PIL import Image
-from wordcloud import WordCloud, STOPWORDS
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+import matplotlib.pyplot as plt
 from io import BytesIO
-import concurrent.futures
 
 app = Flask(__name__)
 
 stopwords = set(STOPWORDS)
 stopwords.update(["drink", "now", "wine", "flavor", "flavors"])
+@app.route('/api/test')
+def test_route():
+    return "Hello, world!"
 
-def transform_format(val):
-    return 255 if val != 0 else val
+@app.route('/api/generate_wordcloud', methods=['POST'])
+def generate_wordcloud():
+    if 'wine_mask' not in request.files:
+        return "No wine mask provided.", 400  # Indicate error with a 400 status code
 
-def one_color_func(word=None, font_size=None, position=None, orientation=None, font_path=None, random_state=None):
-    h, s, l = 0, 0, 0
-    return "hsl({}, {}%, {}%)".format(h, s, l)
-
-def generate_wordcloud_async(text_input, wine_mask):
+    wine_mask = request.files['wine_mask']
     wine_mask_np = np.array(Image.open(wine_mask))
+
+    def transform_format(val):
+        return 255 if val != 0 else val
+
     vectorized_transform_format = np.vectorize(transform_format)
     transformed_wine_mask = vectorized_transform_format(wine_mask_np)
+
+    def one_color_func(word=None, font_size=None, position=None, orientation=None, font_path=None, random_state=None):
+        h, s, l = 0, 0, 0
+        return "hsl({}, {}%, {}%)".format(h, s, l)
+
+    text_input = request.form.get('text_input', '')
 
     wc = WordCloud(
         background_color="white",
@@ -33,29 +44,9 @@ def generate_wordcloud_async(text_input, wine_mask):
 
     wc.generate(text_input.upper())
 
-    # Save the word cloud image to a BytesIO object
     img_buffer = BytesIO()
     wc.to_image().save(img_buffer, format='PNG')
     img_buffer.seek(0)
-
-    return img_buffer
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/generate_wordcloud', methods=['POST'])
-def generate_wordcloud():
-    if 'wine_mask' not in request.files:
-        return "No wine mask provided."
-
-    wine_mask = request.files['wine_mask']
-    text_input = request.form.get('text_input', '')
-
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        # Run the image generation task asynchronously
-        img_future = executor.submit(generate_wordcloud_async, text_input, wine_mask)
-        img_buffer = img_future.result()
 
     return send_file(img_buffer, mimetype='image/png', as_attachment=True, download_name='wordcloud.png')
 
